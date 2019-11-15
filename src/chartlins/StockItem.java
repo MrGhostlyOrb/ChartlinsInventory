5124ddd875d4cf28121df807f9d6f6b3c31c86fb
Ben <bassbencooper999@gmail.com>
2019-11-15 17:14:48 +0000

    Fixed StockItem class and created blueprint for item objects with methods to return all values

--- src/chartlins/StockItem.java
+++ src/chartlins/StockItem.java
@@ -5,21 +5,42 @@ import java.util.Arrays;
 public class StockItem {
     //This class will be used to import the information from the inventory class and sort the information.
 
-    static String[] sortPriceHigh(String[] itemArray){
-        String[] splitArray;
-        for(int i = 1; i < itemArray.length; i++){
-            System.out.println(itemArray[i]);
-            String item;
-            item = itemArray[i];
-            String[] infoArray;
-            infoArray = item.split(",");
-            System.out.println("The item is : ");
-            System.out.println(infoArray[0]);
-            System.out.println("ThePrice for this item is : ");
-            System.out.println(infoArray[3]);
-            //TODO sort these items by highest price in this method
-        }
-        //TODO return string array of all of the items sorted by price
-        return(itemArray);
+    //Fields
+
+    private String product;
+    private String code;
+    private int qty;
+    private int price;
+    private String sp;
+
+    //Construct a Item object
+
+    public StockItem(String productName, String productCode, int quantity, int itemPrice, String special){
+
+        product = productName;
+        code = productCode;
+        qty = quantity;
+        price = itemPrice;
+        sp = special;
+
     }
+
+    //Methods to return object values
+
+    public String getProduct(){
+        return product;
+    }
+    public String getCode(){
+        return code;
+    }
+    public int getQty(){
+        return qty;
+    }
+    public int getPrice(){
+        return price;
+    }
+    public String getSpecial(){
+        return sp;
+    }
+
 }
